34b15e5e77f64c2491a8a6b4a8d79386ac2e859a
Wakeshi Merdassa <99863726+wakiti@users.noreply.github.com>
2025-05-06 14:24:48 -0900

    Update with_signature.sol

--- Code/Lesson 2/Address Interaction/Calling Contracts/with_signature.sol
+++ Code/Lesson 2/Address Interaction/Calling Contracts/with_signature.sol
@@ -1,1 +1,14 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Sidekick {
+    function sendAlert(address hero, uint enemies, bool armed) external {
+        // Pass the proper calldata using abi.encodeWithSignature
+        (bool success, ) = hero.call(
+            abi.encodeWithSignature("alert(uint256,bool)", enemies, armed)
+        );
+
+        require(success, "Alert failed!"); // Ensure the call was successful
+    }
+} 
